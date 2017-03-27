14c487d00054de0de6cb91c457bce2b318e28b0b
janseredynski <janseredynski@gmail.com>
2017-03-28 00:54:38 +0200

    Added searching for connection between two cities based on ryanair database

--- intents/place_intents.py
+++ intents/place_intents.py
@@ -8,24 +8,56 @@ from models.database import Database
 DEPARTURE_CITY = "DEPARTURE_CITY"
 DESTINATION_CITY = "DESTINATION_CITY"
 
+#database = Database.Instance()
+
+# def connections_between_cities(departure_city_name, destination_city_name):
+#     "Returns list of airport codes if true, otherwise None"
+#     dep_codes = database.get_airport_codes(departure_city_name)
+#     des_codes = database.get_airport_codes(destination_city_name)
+#     return database.does_any_connection_exist(dep_codes, des_codes)
+
+
 def handle_place_intents(ask):
     "Place intents handler"
+
     database = Database.Instance()
+
+    def connections_between_cities(departure_city_name, destination_city_name):
+        "Returns list of airport codes if true, otherwise None"
+        dep_codes = database.get_airport_codes(departure_city_name)
+        des_codes = database.get_airport_codes(destination_city_name)
+        return database.does_any_connection_exist(dep_codes, des_codes)
+
+    def start_searching_for_flight():
+        "Finds a flight and decides if can proceed"
+
+        if connections_between_cities(              \
+            session.attributes[DEPARTURE_CITY],     \
+            session.attributes[DESTINATION_CITY]):
+            return statement(                                           \
+                    render_template(                                    \
+                        'destinationAndDepartureCollected').format(     \
+                        session.attributes[DEPARTURE_CITY],             \
+                        session.attributes[DESTINATION_CITY]))
+        else:
+            return statement(                                   \
+                render_template('noFlightConnection').format(   \
+                    session.attributes[DEPARTURE_CITY],         \
+                 session.attributes[DESTINATION_CITY]))
+
+
+
 
     @ask.intent("PlaceIntent")
     def place(name):
-        "If user says just name of the city. Eg.'London' or 'Warsaw' "
-
+        "If user says just the name of the city. Eg.'London'. "    \
+         "Then we need to check what info he already passed"
         destination_is_set = DESTINATION_CITY in session.attributes
 
         if destination_is_set:
             if database.does_place_exist(name):
                 session.attributes[DEPARTURE_CITY] = name
-                return statement(
-                    render_template(                                    \
-                        'destinationAndDepartureCollected').format(     \
-                        session.attributes[DEPARTURE_CITY],             \
-                        session.attributes[DESTINATION_CITY]))
+                return start_searching_for_flight()
             return statement(render_template('noSuchDestination').format(name))
 
         else:
@@ -37,7 +69,8 @@ def handle_place_intents(ask):
 
     @ask.intent("DestinationPlaceIntent")
     def detination_place(name):
-        "Receives destination place         Eg. 'I want to go to London' or 'to London' "
+        "Receives destination place. Eg. 'I want to go to London' or 'to London'."  \
+        " So we need to ask for the departure city"
 
         if database.does_place_exist(name):
             session.attributes[DESTINATION_CITY] = name
@@ -54,11 +87,7 @@ def handle_place_intents(ask):
 
         if database.does_place_exist(name):
             session.attributes[DEPARTURE_CITY] = name
-            return statement(
-                render_template(                                \
-                'destinationAndDepartureCollected').format(     \
-                session.attributes[DEPARTURE_CITY],             \
-                session.attributes[DESTINATION_CITY]))
+            return start_searching_for_flight()
         else:
             msg = render_template('noSuchDestination').format(name)
             return statement(msg)
--- models/database.py
+++ models/database.py
@@ -11,15 +11,15 @@ class Database(object):
     "Shared connection to the database"
 
     def __init__(self):
-        self.cities = ["Warsaw", "Manchester", "Berlin"]
 
         self.flights = pd.read_csv('./models/flight-list.csv')
         self.codes = pd.read_csv('./models/airport-codes.csv', usecols=[9, 11]).dropna()
 
     def does_place_exist(self, destination_name):
-        "Checks if place exists"
-        if destination_name in self.cities:
-            return True
+        "Chcecks if the city has an airport"
+        for index, row in self.codes.iterrows():
+            if row[0] == destination_name:
+                return True
         return False
 
     def get_flights(self, departure_station, arrival_station, departure_date):
@@ -43,3 +43,22 @@ class Database(object):
             if row[0] == city:
                 result.append(row[1])
         return result
+
+    def does_connection_exist(self, departure_station_code, arrival_station_code):
+        "checks if there is any flight between two airports"
+
+        for index, row in self.flights.iterrows():
+            source = row[2]
+            destination = row[3]
+            if source == departure_station_code and destination == arrival_station_code:
+                return True
+        return False
+
+    def does_any_connection_exist(self, departure_station_code_list, arrival_station_code_list):
+        "checks if there is any flight between two airports lists"
+
+        for departure_station_code in departure_station_code_list:
+            for arrival_station_code in arrival_station_code_list:
+                if self.does_connection_exist(departure_station_code, arrival_station_code):
+                    return [departure_station_code, arrival_station_code]
+        return None
